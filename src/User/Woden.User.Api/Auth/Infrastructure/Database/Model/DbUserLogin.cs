2f137ad6f8ae121617686c3afdaa1b2158fd78c3
Kai Goedde <kai@goeddek.de>
2022-08-31 23:22:39 +0200

    # IS(1) 20220831 - database added

---
+++ src/User/Woden.User.Api/Auth/Infrastructure/Database/Model/DbUserLogin.cs
@@ -0,0 +1,6 @@
+﻿namespace Goedde88.Woden.User.Api.Auth.Infrastructure.Database.Model;
+
+public class DbUserLogin : IdentityUserLogin<Guid>
+{
+    public virtual DbUser? User { get; init; }
+}
---
+++ src/User/Woden.User.Api/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Goedde88.Woden.User.Api.Auth.Infrastructure.Database;
+global using Goedde88.Woden.User.Api.Auth.Infrastructure.Database.Model;
+
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
